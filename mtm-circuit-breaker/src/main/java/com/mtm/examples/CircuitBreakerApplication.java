fb8be26bafae4c4dfef13d46e0b49a2216f5cab7
Manjunath M T <itzmtmanju@gmail.com>
2023-05-13 22:25:24 +0530

    Code refactoring

---
+++ mtm-circuit-breaker/src/main/java/com/mtm/examples/CircuitBreakerApplication.java
@@ -0,0 +1,15 @@
+package com.mtm.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ManjunathMT
+ */
+@SpringBootApplication
+public class CircuitBreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitBreakerApplication.class, args);
+    }
+}
