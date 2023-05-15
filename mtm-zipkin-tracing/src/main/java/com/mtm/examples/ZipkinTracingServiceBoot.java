5f7be6505fd2bc0d0d46bfa891a1b985834ba68b
Manjunath M T <itzmtmanju@gmail.com>
2023-05-15 21:30:06 +0530

    ZIpkin dependency changes

--- mtm-zipkin-tracing/src/main/java/com/mtm/examples/ZipkinTracingServiceBoot.java
+++ mtm-zipkin-tracing/src/main/java/com/mtm/examples/ZipkinTracingServiceBoot.java
@@ -2,7 +2,7 @@ package com.mtm.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
